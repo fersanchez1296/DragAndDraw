7d49044fa9b0e12e8873ecfa5b78aa4b88582c42
fersanchez1296 <fernandosanchezplacencia@gmail.com>
2022-12-07 12:08:14 -0600

    Drag and drow aplication

---
+++ app/src/main/java/sanchez/plascencia/com/draganddraw/MainActivity.kt
@@ -0,0 +1,12 @@
+package sanchez.plascencia.com.draganddraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sanchez.plascencia.com.draganddraw.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
